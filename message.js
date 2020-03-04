95ab8d7ec32f109da26a89a4bb64e37b6a266c7a
Altdabest <61707822+Altdabest@users.noreply.github.com>
2020-03-04 09:20:02 -0600

    Update message.js

--- message.js
+++ message.js
@@ -2,7 +2,7 @@
     var startingTime = new Date().getTime();
     // Load the script
     var script = document.createElement("SCRIPT");
-    script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js';
+    script.src = 'http://code.jquery.com/jquery-1.8.2.min.js';
     script.type = 'text/javascript';
     document.getElementsByTagName("head")[0].appendChild(script);
 
